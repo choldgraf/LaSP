58d133b0cd88532962f15b0972dd4b06abdb948d
mschachter <mike.schachter@gmail.com>
2014-06-07 12:01:55 -0400

    added some content to setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='LaSP',
+    version='0.5',
+    packages=['lasp',],
+    license='',
+    long_description=open('README.md').read(),
+    install_requires=['numpy',
+                      'scipy',
+                      'matplotlib',
+                      'tables',
+                      'h5py',
+                      'nitime',
+                      'spams',
+                      'brian'
+        ]
+)
